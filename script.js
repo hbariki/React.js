d4dc5e88222dab044807c6c28474a16467032986
Harshini <harshinibariki@gmail.com>
2017-07-02 12:07:24 -0700

    Refactor : Add handle click component to increment the button

--- script.js
+++ script.js
@@ -2,11 +2,23 @@
  * Created by nmekala on 7/1/17.
  */
 class Button extends React.Component {
-    state = { counter : 0 };
-    render() {
-        return (
-            <button>41</button>
-        );
-    }
+    state = { counter : 0}
+    // we can define it as a component as instance property
+    handleClick = () => {
+    // this == component instance, handle click reads the current counter by using {this.state.counter++}
+    this.setState((prevState) => ({
+    counter: prevState.counter + 1
+}));
+
+};
+
+render() {
+    return(
+        <button onClick={this.handleClick}>
+    {this.state.counter}
+</button>
+);
 }
-ReactDOM.render(<Button /> , mountNode);
+}
+
+ReactDOM.render(<Button />, mountNode);
